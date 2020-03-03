a12e177f9bc500b218b38b7172f8918f4d791d5b
sonsoleslp <poynting.slp@gmail.com>
2020-03-03 12:59:08 +0100

    Add domain name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ role :db,  %w{ubuntu@138.4.22.160}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '138.4.22.160', user: 'ubuntu', roles: %w{web app}, my_property: :my_value
+server 'myjson.dit.upm.es', user: 'ubuntu', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
